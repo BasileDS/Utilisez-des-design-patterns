19ae51a86163b0d2bbdfdd6ff26bb05b2536a7ef
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-04-30 16:38:00 +0200

    Oldmovie constructor

--- js/models/OldMovie.js
+++ js/models/OldMovie.js
@@ -1,3 +1,33 @@
 class OldMovie {
+  constructor(data) {
+    this._title = data.title
+    this._synopsis = data.synopsis
+    this._picture = data.picture
+    this._duration = data.duration
+    this._release_in = data.release_in
+  }
 
+  get title() {
+    return this._title
+  }
+
+  get synopsis() {
+    return this._synopsis
+  }
+
+  get duration() {
+    return this._duration
+  }
+
+  get release_in() {
+    return this._release_in
+  }
+
+  get picture() {
+    return `/assets/${this._picture}`
+  }
+
+  get thumbnail() {
+    return `/assets/thumbnails/${this._picture}`
+  }
 }
