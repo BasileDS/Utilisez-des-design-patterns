85015e74809e827d69c01f62c1c75f1283a176d7
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-05-03 10:39:29 +0200

    Adapter Patten Partie 3 Chap 1

--- js/adapters/FilterMoviesAdapter.js
+++ js/adapters/FilterMoviesAdapter.js
@@ -1,2 +1,9 @@
 class FilterMoviesAdapter {
+  constructor (Movies, actor) {
+    this._Movies = Movies
+    this._actor = actor
+  }
+  async filterByActor() {
+    return await FilterV2.filterByActor(this._actor, this._Movies)
+  }
 }
